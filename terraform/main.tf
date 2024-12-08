e514d452e3447e4d1e4db09136e7ba9abdf2080b
Kevin Peña <kevin.pena.prog@gmail.com>
2024-12-08 14:15:28 -0500

    Terraform changes for vpn-wireguard playbook

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,77 @@
+variable "playbook" {
+  default = "nginx-docker"
+}
+
+variable "volume_size" {
+  default = 10
+}
+
 locals {
-  playbook = "nginx-docker"
+  ports = {
+    nginx-docker = {
+      http  = { port = 80, protocol = "tcp" }
+      https = { port = 443, protocol = "tcp" }
+    }
+    vpn-l2tp = {
+      vpn1 = { port = 500, protocol = "udp" }
+      vpn2 = { port = 4500, protocol = "udp" }
+      vpn3 = { port = 1701, protocol = "udp" }
+    }
+    vpn-wireguard = {
+      vpn = { port = 51820, protocol = "udp" }
+    }
+  }[var.playbook]
+
+  os_details = {
+    al23 = {
+      ami  = data.aws_ami.al23_latest.id
+      user = "ec2-user"
+    }
+    ubuntu = {
+      ami  = data.aws_ami.ubuntu_24.id
+      user = "ubuntu"
+    }
+  }
+
+  os = {
+    nginx-docker  = local.os_details.al23
+    vpn-l2tp      = local.os_details.ubuntu
+    vpn-wireguard = local.os_details.ubuntu
+  }[var.playbook]
 }
 
 resource "aws_instance" "main" {
-  ami                         = data.aws_ami.al23_latest.id
+  ami                         = local.os.ami
   instance_type               = "t3.micro"
   subnet_id                   = data.aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.app.id]
   associate_public_ip_address = true
+  key_name                    = aws_key_pair.main.key_name
+
+  root_block_device {
+    volume_size = var.volume_size
+  }
 
   user_data = <<-EOT
     #cloud-config
     packages:
       - git
-    ansible:
-      install_method: distro
-      package_name: ansible
-      pull:
-        url: https://github.com/kebien6020/cloud-init-data.git
-        playbook_name: ansible/${local.playbook}/playbook.yml
+      - ansible
+    package_update: true
+    package_upgrade: true
+    runcmd:
+      - mkdir -p /run/init
+      - ansible-pull -U https://github.com/kebien6020/cloud-init-data.git -d /run/init -vvv ansible/${var.playbook}/playbook.yml
   EOT
 
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_key_pair" "main" {
+  key_name   = "cloud-init-data-main"
+  public_key = file("pub_key.pub")
 }
 
 output "public_ip" {
@@ -34,6 +82,10 @@ output "instance_id" {
   value = aws_instance.main.id
 }
 
+output "ssh_command" {
+  value = "ssh ${local.os.user}@${aws_instance.main.public_ip}"
+}
+
 data "aws_ami" "al23_latest" {
   owners      = ["amazon"]
   most_recent = true
@@ -41,6 +93,21 @@ data "aws_ami" "al23_latest" {
   filter {
     name   = "name"
     values = ["al2023-ami-minimal-*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_ami" "ubuntu_24" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
   filter {
@@ -67,18 +134,10 @@ resource "aws_security_group" "app" {
   name   = "app-sg"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "http" {
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
   security_group_id = aws_security_group.app.id
-  from_port         = 80
-  to_port           = 80
-  ip_protocol       = "tcp"
-  cidr_ipv4         = "0.0.0.0/0"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "https" {
-  security_group_id = aws_security_group.app.id
-  from_port         = 443
-  to_port           = 443
+  from_port         = 22
+  to_port           = 22
   ip_protocol       = "tcp"
   cidr_ipv4         = "0.0.0.0/0"
 }
@@ -86,5 +145,15 @@ resource "aws_vpc_security_group_ingress_rule" "https" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.app.id
   ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "service_ports" {
+  for_each = local.ports
+
+  security_group_id = aws_security_group.app.id
+  from_port         = each.value.port
+  to_port           = each.value.port
+  ip_protocol       = each.value.protocol
   cidr_ipv4         = "0.0.0.0/0"
 }
